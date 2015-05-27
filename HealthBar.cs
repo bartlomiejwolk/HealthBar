f723215cd3d321ade20c5f0f6c01161913a31f53
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-27 08:56:57 +0200

    Encapsulate fields

--- HealthBar.cs
+++ HealthBar.cs
@@ -24,12 +24,40 @@ namespace HealthbarEx {
         // the camera used to track health bar size
         public Transform cameraTracker;
         public float refRelativeDist = 5;
+        #endregion
+
+        #region PROPERTIES
+
+        public float HealthValue {
+            get { return healthValue; }
+            set { healthValue = value; }
+        }
+
+        public HealthBarGUI HealthBarGui {
+            get { return healthBarGUI; }
+            set { healthBarGUI = value; }
+        }
+
+        public Vector3 TargetOffset {
+            get { return targetOffset; }
+            set { targetOffset = value; }
+        }
+
+        public Transform CameraTracker {
+            get { return cameraTracker; }
+            set { cameraTracker = value; }
+        }
+
+        public float RefRelativeDist {
+            get { return refRelativeDist; }
+            set { refRelativeDist = value; }
+        }
 
         #endregion
 
         #region UNITY MESSAGES
         void OnGUI() {
-            if (!healthBarGUI.texture) {
+            if (!HealthBarGui.texture) {
                 Debug.Log("ERROR: Health bar texture has not been assigned!");
                 return;
             }
@@ -40,21 +68,21 @@ namespace HealthbarEx {
                 Vector2 barPos =
                     Camera.main.WorldToScreenPoint(
                             transform.position +
-                            targetOffset);
-                DrawHealthBar(barPos + healthBarGUI.offset);
+                            TargetOffset);
+                DrawHealthBar(barPos + HealthBarGui.offset);
             }
         }
 
 
         private void Start () {
             // todo remove health field
-            health = healthValue;
+            health = HealthValue;
             currentValue = health;
             maxHealth = health;
         }
 
         private void Update () {
-            health = healthValue;
+            health = HealthValue;
             HealthBarController();
         }
 
@@ -72,13 +100,13 @@ namespace HealthbarEx {
         }
         IEnumerator DisplayHealthBar (float target){
             float timer = 0f;
-            healthBarGUI.alpha = healthBarGUI.visibility;
-            while (healthBarGUI.alpha > 0f){
-                if (timer > healthBarGUI.transitionDelay){
-                    healthBarGUI.alpha = Mathf.MoveTowards(
-                        healthBarGUI.alpha,
+            HealthBarGui.alpha = HealthBarGui.visibility;
+            while (HealthBarGui.alpha > 0f){
+                if (timer > HealthBarGui.transitionDelay){
+                    HealthBarGui.alpha = Mathf.MoveTowards(
+                        HealthBarGui.alpha,
                         0f,
-                        Time.deltaTime * healthBarGUI.transitionSpeed);
+                        Time.deltaTime * HealthBarGui.transitionSpeed);
                 }
                 timer += Time.deltaTime;
                 yield return null;
@@ -89,97 +117,97 @@ namespace HealthbarEx {
         private void DrawHealthBar (Vector2 barPos) {
         // scale the gui if camera is available
             float GUIScale = 0;
-            if (cameraTracker != null){
-                float cameraDist = (cameraTracker.position-transform.position).magnitude;
-                GUIScale = refRelativeDist/Cut(cameraDist, 0.001f, Mathf.Infinity);
+            if (CameraTracker != null){
+                float cameraDist = (CameraTracker.position-transform.position).magnitude;
+                GUIScale = RefRelativeDist/Cut(cameraDist, 0.001f, Mathf.Infinity);
             } else
                 GUIScale = 1;
         // Holds health bar lerped value.
             currentValue = Mathf.Lerp(
                 currentValue,
                 Cut(health, 0, Mathf.Infinity),
-                Time.deltaTime * healthBarGUI.animationSpeed);
+                Time.deltaTime * HealthBarGui.animationSpeed);
             // Destination.
             float barWidth =
-                (healthBarGUI.width * GUIScale) * (Cut(health, 0, Mathf.Infinity) / maxHealth);
+                (HealthBarGui.width * GUIScale) * (Cut(health, 0, Mathf.Infinity) / maxHealth);
             // Learped value.
             float lerpBarWidth =
-                (healthBarGUI.width * GUIScale) * (currentValue / maxHealth);
+                (HealthBarGui.width * GUIScale) * (currentValue / maxHealth);
         // switch the position modes
             float horizPos_available = 0f;
             float horizPos_variable = 0f;
-            switch (healthBarGUI.positionMode){
+            switch (HealthBarGui.positionMode){
                 case HealthBarGUI.PositionModes.Fixed:
                     // White.
                     horizPos_available =
-                        barPos.x + healthBarGUI.offset.x * Mathf.Pow(GUIScale, 2);
+                        barPos.x + HealthBarGui.offset.x * Mathf.Pow(GUIScale, 2);
                     // Red.
                     horizPos_variable =
-                        barPos.x + healthBarGUI.offset.x * Mathf.Pow(GUIScale, 2) + barWidth;
+                        barPos.x + HealthBarGui.offset.x * Mathf.Pow(GUIScale, 2) + barWidth;
                 break;
                 case HealthBarGUI.PositionModes.Center:
                     horizPos_available =
-                        barPos.x + healthBarGUI.offset.x * GUIScale - (barWidth * 0.5f);
+                        barPos.x + HealthBarGui.offset.x * GUIScale - (barWidth * 0.5f);
                     horizPos_variable =
-                        barPos.x + healthBarGUI.offset.x * GUIScale - (barWidth * 0.5f) + barWidth;
+                        barPos.x + HealthBarGui.offset.x * GUIScale - (barWidth * 0.5f) + barWidth;
                 break;
             }
         // draw health bar background on fixed position
-            if (healthBarGUI.positionMode == HealthBarGUI.PositionModes.Fixed){
-                GUI.color = new Color(0.8f, 0.8f, 0.8f, healthBarGUI.alpha * 0.5f);
+            if (HealthBarGui.positionMode == HealthBarGUI.PositionModes.Fixed){
+                GUI.color = new Color(0.8f, 0.8f, 0.8f, HealthBarGui.alpha * 0.5f);
                 GUI.DrawTexture(
                     new Rect(
-                        barPos.x + healthBarGUI.offset.x * Mathf.Pow(GUIScale, 2),
-                        Screen.height - barPos.y - healthBarGUI.offset.y,
-                        healthBarGUI.width * GUIScale,
-                        healthBarGUI.height * GUIScale),
-                    healthBarGUI.texture, 
+                        barPos.x + HealthBarGui.offset.x * Mathf.Pow(GUIScale, 2),
+                        Screen.height - barPos.y - HealthBarGui.offset.y,
+                        HealthBarGui.width * GUIScale,
+                        HealthBarGui.height * GUIScale),
+                    HealthBarGui.texture, 
                     ScaleMode.StretchToFill,
                     true);
             }
         // Draw the "current health" part of the health bar
-            GUI.color = healthBarGUI.availableHealth;
+            GUI.color = HealthBarGui.availableHealth;
             GUI.color =
-                new Color(GUI.color.r, GUI.color.g, GUI.color.b, healthBarGUI.alpha);
+                new Color(GUI.color.r, GUI.color.g, GUI.color.b, HealthBarGui.alpha);
             GUI.DrawTexture(
                 new Rect(
                     horizPos_available,
-                    Screen.height - barPos.y - healthBarGUI.offset.y,
+                    Screen.height - barPos.y - HealthBarGui.offset.y,
                     barWidth,
-                    healthBarGUI.height * GUIScale),
-                healthBarGUI.texture, 
+                    HealthBarGui.height * GUIScale),
+                HealthBarGui.texture, 
                 ScaleMode.StretchToFill,
                 true);
         // Draw the animated "decrease" part of the health bar
             if (currentValue > previousValue)
-                GUI.color = healthBarGUI.drainedHealth;
+                GUI.color = HealthBarGui.drainedHealth;
             else
-                GUI.color = healthBarGUI.addedHealth;
+                GUI.color = HealthBarGui.addedHealth;
             GUI.color =
-                new Color(GUI.color.r, GUI.color.g, GUI.color.b, healthBarGUI.alpha);
+                new Color(GUI.color.r, GUI.color.g, GUI.color.b, HealthBarGui.alpha);
             GUI.DrawTexture(
                 new Rect(
                     horizPos_variable,
-                    Screen.height - barPos.y - healthBarGUI.offset.y,
+                    Screen.height - barPos.y - HealthBarGui.offset.y,
                     lerpBarWidth - barWidth,
-                    healthBarGUI.height * GUIScale),
-                healthBarGUI.texture, 
+                    HealthBarGui.height * GUIScale),
+                HealthBarGui.texture, 
                 ScaleMode.StretchToFill, 
                 true);
         // Draw the value
-            if (healthBarGUI.displayValue){
-                GUI.color = healthBarGUI.displayedValue;
+            if (HealthBarGui.displayValue){
+                GUI.color = HealthBarGui.displayedValue;
                 GUI.color =
-                    new Color(GUI.color.r, GUI.color.g, GUI.color.b, healthBarGUI.alpha);
+                    new Color(GUI.color.r, GUI.color.g, GUI.color.b, HealthBarGui.alpha);
                 GUI.Label(
                     new Rect(
-                        horizPos_available + healthBarGUI.valueOffset.x, 
+                        horizPos_available + HealthBarGui.valueOffset.x, 
                         Screen.height - barPos.y - 48 +
-                        healthBarGUI.offset.y - healthBarGUI.valueOffset.y - healthBarGUI.height,
+                        HealthBarGui.offset.y - HealthBarGui.valueOffset.y - HealthBarGui.height,
                         30,
                         20), 
                     currentValue.ToString("#"),
-                    healthBarGUI.textStyle);
+                    HealthBarGui.textStyle);
             }
         }
 
